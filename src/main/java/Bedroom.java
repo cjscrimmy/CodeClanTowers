aaabe117c1a9b98e1cc1f180a2bfdf33c746bab7
Nathan <nathankirley@gmail.com>
2020-06-17 11:26:05 +0100

    hasCapacity and hasRoomNumber both pass

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,4 +1,13 @@
 public class Bedroom {
     private int roomNumber;
+    private int capacity;
+    private String type;
 
+    public Bedroom(int roomNumber, int capacity, String type){
+        this.roomNumber = roomNumber;
+    }
+
+    public int getRoomNumber(){
+        return this.roomNumber;
+    }
 }
