3ece1607a0f9a84e2b3410b3c856239d4f958685
Nathan <nathankirley@gmail.com>
2020-06-17 14:30:29 +0100

    hasBedroom, hasNumberOfNights, canAddNight all pass

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,2 +1,22 @@
 public class Booking {
+
+    private Bedroom bedroom;
+    private int numberOfNights;
+
+    public Booking(Bedroom bedroom, int numberOfNights){
+        this.bedroom = bedroom;
+        this.numberOfNights = numberOfNights;
+    }
+
+    public int getNumberOfNights(){
+        return this.numberOfNights;
+    }
+
+    public Bedroom getBedroom(){
+        return this.bedroom;
+    }
+
+    public void addNights(int number){
+        this.numberOfNights += number;
+    }
 }
--- src/test/java/BookingTest.java
+++ src/test/java/BookingTest.java
@@ -5,13 +5,14 @@ import static org.junit.Assert.assertEquals;
 
 public class BookingTest {
 
-    private Booking booking1;
     private Bedroom bedroom1;
+    private Booking booking1;
 
     @Before
     public void before(){
-        booking1 = new Booking(bedroom1, 2);
         bedroom1 = new Bedroom(23, 2, "double");
+        booking1 = new Booking(bedroom1, 2);
+//        booking1.addBedroom(bedroom1);
     }
 
     @Test
@@ -21,12 +22,12 @@ public class BookingTest {
 
     @Test
     public void hasNumberOfNights(){
-        assertEquals(2, booking1.numberOfNights());
+        assertEquals(2, booking1.getNumberOfNights());
     }
 
     @Test
     public void canAddNight(){
         booking1.addNights(3);
-        assertEquals(3, booking1.numberOfNights());
+        assertEquals(5, booking1.getNumberOfNights());
     }
 }
